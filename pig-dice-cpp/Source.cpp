870a6673602759733853dee2d8ab939b9bacfcaf
Matthew Kirkendall <kirk.viola@gmail.com>
2022-03-23 23:16:14 -0400

    pig dice works

--- pig-dice-cpp/Source.cpp
+++ pig-dice-cpp/Source.cpp
@@ -6,12 +6,35 @@ using namespace std;
 int main() {
 
 	int target;
+	cout << "Enter your target high score: ";
+	cin >> target;
+	int highScore = 0;
+	int currentScore;
+	int roll;
+	long repCounter = 0;
 
+	 
 	srand(time(NULL));
 
-	for (int n = 0; n != 10; ++n) {
+	while (highScore < target)
+	{
+		++repCounter;
+		if (repCounter % 100000000 == 0) {
+			srand(time(NULL));
+			cout << "Current repetitions: " << repCounter << endl;
+		}
+		currentScore = 0;
+		do {
+			roll = rand() % 6 + 1;
+			currentScore += roll;
+			if (currentScore > highScore) {
+				highScore = currentScore;
+				cout << "New high score! " << highScore << endl;
+			}
+		} while (roll != 1);
+
+	} 
 
 
-	}
-
+	return 0;
 }
